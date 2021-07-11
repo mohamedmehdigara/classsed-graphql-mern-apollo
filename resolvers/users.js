a2298edbd7ab91c3ad521fcca7c969d3d7f9883b
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-07-11 15:34:34 +0100

    added files

---
+++ resolvers/users.js
@@ -0,0 +1,29 @@
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jasonwebtoken");
+const { SECRET_KEY } = require("../../config");
+
+module.exports = {
+  Mutation: {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      context,
+      info
+    ) {
+      password = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+      const res = await newUser.save();
+      const token = jwt.sign({
+        id: res.id,
+        email: res.email,
+        username: res.username,
+      }, {});
+    },
+  },
+};
