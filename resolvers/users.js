7f1962120ea93a84f53fff5ed7b1c994d652d6a7
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-07-11 15:59:56 +0100

    added files

--- resolvers/users.js
+++ resolvers/users.js
@@ -19,11 +19,20 @@ module.exports = {
         createdAt: new Date().toISOString(),
       });
       const res = await newUser.save();
-      const token = jwt.sign({
-        id: res.id,
-        email: res.email,
-        username: res.username,
-      }, {});
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
