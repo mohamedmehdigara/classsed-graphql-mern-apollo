18cdf3e084f0e96c45231e1f4bf83c28a684104c
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-07-11 12:56:22 +0100

    added files

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
-const mongoose = require('mongoose');
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+const mongoose = require("mongoose");
 
-const Post = require('./models/Post');
-const { MONGODB } = require('./config.js');
+const Post = require("./models/Post");
+const { MONGODB } = require("./config.js");
 
-const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
-`;
+
 
 const resolvers = {
   Query: {
@@ -26,19 +16,19 @@ const resolvers = {
       } catch (err) {
         throw new Error(err);
       }
-    }
-  }
+    },
+  },
 };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
     return server.listen({ port: 5000 });
   })
   .then((res) => {
